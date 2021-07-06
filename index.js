1b8d9ea0839a32ace28958117413a5eaa4f4d08d
dkbom acer labtop <dkbom023@hotmail.com>
2021-07-06 13:59:18 +0700

    finish

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const ifLoggedIn = (req, res, next) => {
 //root
 app.get("/", ifNotLoggedIn, (req, res, next) => {
   dbConnection
-    .execute("SELECT name FROM users WHERE id =?"[req.session.userID])
+    .execute("SELECT name FROM users WHERE id =?", [req.session.userID])
     .then(([rows]) => {
       res.render("home", {
         name: rows[0].name,
@@ -48,20 +48,131 @@ app.get("/", ifNotLoggedIn, (req, res, next) => {
 });
 
 // Register Page
-app.post("/register", ifLoggedIn, [
-  body("user_email", "Invalid Email Address!")
-    .isEmail()
-    .custom((value) => {
+app.post(
+  "/register",
+  ifLoggedIn,
+  [
+    body("user_email", "Invalid Email Address!")
+      .isEmail()
+      .custom((value) => {
+        return dbConnection
+          .execute("SELECT email FROM users WHERE email =?", [value])
+          .then(([rows]) => {
+            if (rows.length > 0) {
+              return Promise.reject("This email is already use!");
+            }
+            return true;
+          });
+      }),
+    body("user_name", "Username is empty!").trim().not().isEmpty(),
+    body("user_pass", "The password must be of minimum length 6 characters")
+      .trim()
+      .isLength({ min: 6 }),
+  ],
+  (req, res, next) => {
+    const validation_result = validationResult(req);
+    const { user_name, user_pass, user_email } = req.body;
+
+    if (validation_result.isEmpty()) {
+      bcrypt
+        .hash(user_pass, 12)
+        .then((hash_pass) => {
+          dbConnection
+            .execute(
+              "INSERT INTO `users`(`name`,`email`,`password`) VALUES(?,?,?)",
+              [user_name, user_email, hash_pass]
+            )
+            .then((result) => {
+              res.send(
+                `your account has been created successfully, Now you can <a href="/">Login</a>`
+              );
+            })
+            .catch((err) => {
+              if (err) throw err;
+            });
+        })
+        .catch((err) => {
+          if (err) throw err;
+        });
+    } else {
+      let allErrors = validation_result.errors.map((error) => {
+        return error.msg;
+      });
+
+      res.render("login-register", {
+        register_error: allErrors,
+        old_data: req.body,
+      });
+    }
+  }
+);
+
+app.post(
+  "/",
+  ifLoggedIn,
+  [
+    body("user_email").custom((value) => {
       return dbConnection
-        .execute("SELECT email FROM users WHERE email =?", [value])
+        .execute("SELECT email FROM users WHERE email=?", [value])
         .then(([rows]) => {
-          if (rows.length > 0) {
-            return Promise.reject("This email is already use!");
+          if (rows.length == 1) {
+            return true;
           }
-          return true;
+          return Promise.reject("Invalid Email Address!");
         });
     }),
-  body("user_name", "Username is empty!").trim().not().isEmpty(),
-]);
+    body("user_pass", "Password is empty!").trim().not().isEmpty(),
+  ],
+  (req, res) => {
+    const validation_result = validationResult(req);
+    const { user_pass, user_email } = req.body;
+    if (validation_result.isEmpty()) {
+      dbConnection
+        .execute("SELECT * FROM `users` WHERE `email`=?", [user_email])
+        .then(([rows]) => {
+          bcrypt
+            .compare(user_pass, rows[0].password)
+            .then((compare_result) => {
+              if (compare_result === true) {
+                req.session.isLoggedIn = true;
+                req.session.userID = rows[0].id;
+
+                res.redirect("/");
+              } else {
+                res.render("login-register", {
+                  login_errors: ["Invalid Password!"],
+                });
+              }
+            })
+            .catch((err) => {
+              if (err) throw err;
+            });
+        })
+        .catch((err) => {
+          if (err) throw err;
+        });
+    } else {
+      let allErrors = validation_result.errors.map((error) => {
+        return error.msg;
+      });
+
+      res.render("login-register", {
+        login_errors: allErrors,
+      });
+    }
+  }
+);
+
+// LOGOUT
+app.get("/logout", (req, res) => {
+  //session destroy
+  req.session = null;
+  res.redirect("/");
+});
+// END OF LOGOUT
+
+app.use("/", (req, res) => {
+  res.status(404).send("<h1>404 Page Not Found!</h1>");
+});
 
 app.listen(3000, () => console.log("Server is running..."));
